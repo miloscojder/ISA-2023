ab46ceaa3d791dea436963a53308547961cdae40
miloscojder <miloscojder@gmail.com>
2023-11-14 19:37:33 +0100

    Inicijalni projekat

---
+++ ISA-proj/src/main/java/com/example/ISAproj/IsaProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.ISAproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsaProjApplication.class, args);
+	}
+
+}
